8c63b242dfefeb02dccc937d36fa7f9271986fe4
cubOlson <cub.olson@gmail.com>
2022-08-31 10:27:39 -0600

    Pre merge

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const express = require("express");
+var cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 let dataArray = ["No info to report"];
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -14,9 +19,18 @@ app.get("/api", (req, res) => {
 });
 
 app.post("/api", (req, res) => {
-  console.log(`Post made with ${req.body}`)
-  dataArray = req.body
-  res.redirect('/api')
+  console.log(req.url)
+  var finalArr = []
+  const {soundArr} = req.body
+  dataArray = soundArr
+  for(let i = 0; i < dataArray.length; i+=25){
+    var loopArr = []
+    for(let j = 0; j < 25; j++){
+      loopArr.push(dataArray[i+j])
+    }
+    finalArr.push(loopArr)
+  }
+  console.log(finalArr)
 });
 
 app.get('*', (req, res) => {
